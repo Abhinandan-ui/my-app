104b28621e64461604b0acd981253574806be5fc
Abhinandan Mundey <77164743+Abhinandan-ui@users.noreply.github.com>
2023-03-02 19:38:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React,{useState} from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import About from './components/About';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+
+  const [mode , setMode] = useState('light');
+  const [alert,setAlert] =useState(null);
+  
+  const   showAlert=(message , type)=>{
+   setAlert({
+    msg: message,
+    type : type
+   })
+   setTimeout(() => {
+     setAlert(null);
+   }, 1500); 
+  }
+
+  
+ const toogleMode =() =>{
+  if(mode === 'light'){
+  setMode('dark');
+ document.body.style.backgroundColor ='rgb(3 13 20)';
+ showAlert("Dark Mode has been enabled","success")
+  }
+  else {
+    setMode('light');
+    document.body.style.backgroundColor ='white';
+    showAlert("Light Mode has been enabled","success")
+  }
+ }
+ return (
+<>
+<Navbar title ="TextUtils" mode = {mode} AboutText="About "   toogleMode={toogleMode} />
+
+  <Alert alert = {alert}/>
+  <div className="container my-3">
+    
+
+          <TextForm  showAlert={showAlert} heading ="Enter the text to analyse" mode={mode}/>
+      
+  </div>
+ 
+
+</>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+   
+  const handleUpClick =() => {
+    //console.log("UpperCase was clicked" + text);
+    let newText =text.toUpperCase();
+    setText(newText)
+ 
+  }
+ 
+  const handleOnChange =(event) =>{
+    setText(event.target.value)
+  }
+ 
+  const  handleloClick =() =>{
+    let lowercase=text.toLowerCase();
+    setText(lowercase)
+   
+  }
+
+  const handleClearClick =() =>{
+    setText("");
+    
+  }
+
+
+  const speak =() =>{
+  let msg = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(msg);
+  const toogle = document.getElementById('toogle')
+  if(toogle.textContent ==="Speak"){
+    toogle.innerHTML ="Stop"
+  }
+
+  else{
+    toogle.innerHTML ="Speak"
+    if(toogle.innerHTMl = "Speak"){
+      window.speechSynthesis.cancel()
+
+      }
+    }
+  }
+  
+  const handleExtraSpaces =() =>{
+    let newText =text.split(/[ ]+/)
+    setText(newText.join(" "))
+   
+  } 
+  
+  const[text , setText] = useState('');
+  
+  return (
+    <>
+    <div class="container" style ={{color: props.mode === 'dark' ? 'white':'black'}} >
+   <h1>{props.heading}  </h1>
+<div class="mb-3">
+  
+  <textarea class="form-control" value ={text} onChange={handleOnChange} style ={{backgroundColor: props.mode === 'dark' ? 'rgb(3 13 20)':'white', color:props.mode === 'dark' ? 'white':'black'}}id="myBox" rows="8"></textarea>
+</div>
+<button class="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+<button class="btn btn-primary mx-2" onClick={handleloClick}>Convert to lowerCase</button>
+<button class="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
+<button class="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Space </button>
+<button class="btn btn-primary mx-2" onClick={speak} id="toogle">Speak</button>
+    </div>
+    <div class="container my-3 "style ={{color: props.mode === 'dark' ? 'white':'black'}}>
+      <h2>Your text summary</h2>
+      <p>{text.split(" ").length} words and  {text.length} characters</p>
+      <p> {0.008 *text.split(" ").length} Minutes read </p>
+    
+    <h2>Preview</h2>
+    <p>{text.length>0?text:"Enter Something in the above textbox to preview here!"}</p>
+    </div>
+    </>
+  )
+}
